ea9a63dbf11eb1e0fe961028d3f1330f8a1bc0f7
moraisacr <66408298+moraisacr@users.noreply.github.com>
2021-03-27 07:26:53 -0300

    primeiro commit

---
+++ Unidade01/src/teste.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		//declaracao de variavel
+		int num1,num2,soma;
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite outro numero");
+		num2 = sc.nextInt();
+		
+		//somar
+		soma = num1 + num2;
+		
+		//mostrar na tela
+		System.out.println("Soma: + soma");
+}
+
+
+}
+
