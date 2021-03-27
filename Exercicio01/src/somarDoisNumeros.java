483e544963ecf2f9e71441e77b0f0816820aafa6
moraisacr <66408298+moraisacr@users.noreply.github.com>
2021-03-27 08:00:04 -0300

    Add files via upload
    2 commit

---
+++ Exercicio01/src/somarDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class somarDoisNumeros {
+public static Scanner sc = new Scanner(System.in);
+
+public static void main (String args[]){
+
+	//Declaracao de variaveis
+	int num1, num2, soma;
+	//Leituras
+	System.out.println("Digite um número");
+	num1 = sc.nextInt();
+	System.out.println("Digite outro número");
+	num2 = sc.nextInt();
+	//Somar
+	soma = num1 + num2;
+	//Mostrar na tela
+	System.out.println("Soma:" + soma);
+	
+	}
+
+}
